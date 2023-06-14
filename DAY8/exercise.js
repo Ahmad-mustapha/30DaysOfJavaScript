0254905a494d8bc9dccfb7c8757abbeae3aba9c7
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-06-14 17:52:28 +0100

    fixes

--- DAY8/exercise.js
+++ DAY8/exercise.js
@@ -83,7 +83,7 @@ const users = {
     },
     Paul: {
       email: 'paul@paul.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node.js'],
       age: 20,
       isLoggedIn: false,
       points: 40
@@ -97,3 +97,125 @@ console.log(users.Daniel.skills.length)
 console.log(users.John.skills.length)
 console.log(users.Thomas.skills.length)
 console.log(users.Paul.skills.length)
+
+
+
+// let maxSkills = 0;
+// let personWithMaxSkills = '';
+
+// Iterate through each user in the `users` object
+// for (const user of Object.keys(users)) {
+  // Access the skills array for the current user
+  // const { skills } = users[user];
+
+  // Check if the number of skills for the current user is greater than the current maximum
+  // if (skills.length > maxSkills) {
+    // If it is, update the maximum skills count and the person with the most skills
+//     maxSkills = skills.length;
+//     personWithMaxSkills = user;
+//   }
+// }
+
+// Print the person with the most skills
+// console.log(personWithMaxSkills);
+
+console.log(Object.entries(users))
+
+
+let maxSkills = 0;
+let personWithMaxSkills;
+
+for (const [user, userDetails] of Object.entries(users)) {
+  const skillsCount = userDetails.skills.length;
+  if (skillsCount > maxSkills) {
+    maxSkills = skillsCount;
+    personWithMaxSkills = user;
+  }
+}
+
+console.log(personWithMaxSkills);
+
+
+/**
+ * 
+ 
+Apologies for the confusion earlier. Since the for...of loop is used to iterate over iterable objects like arrays, we can't directly use it to iterate over the properties of an object. However, we can still achieve the desired result by converting the object properties into an iterable format.
+
+Here's an example of how you can use a for...of loop with the Object.entries() method to iterate over the users object:
+
+javascript
+Copy code
+let maxSkills = 0;
+let personWithMaxSkills;
+
+for (const [user, userDetails] of Object.entries(users)) {
+  const skillsCount = userDetails.skills.length;
+  if (skillsCount > maxSkills) {
+    maxSkills = skillsCount;
+    personWithMaxSkills = user;
+  }
+}
+
+console.log(personWithMaxSkills);
+Let's go through the code step by step:
+
+We initialize the maxSkills variable to 0 and personWithMaxSkills to undefined, just like before.
+
+The for...of loop is used with Object.entries(users). This converts the users object into an array of key-value pairs, where each pair is an array [key, value] representing a property and its corresponding value.
+
+During each iteration of the loop, the current key-value pair is destructured into the variables user and userDetails. The user variable represents the name of the person, and the userDetails variable holds the corresponding user object.
+
+Within the loop, we calculate the number of skills for the current user by accessing userDetails.skills.length. This gives us the length of the skills array for that user.
+
+We compare the skillsCount with the current maximum number of skills (maxSkills). If skillsCount is greater than maxSkills, we update maxSkills to skillsCount and assign the current user to personWithMaxSkills.
+
+After the loop finishes, the personWithMaxSkills variable will hold the name of the person who has the most skills.
+
+Finally, we log the value of personWithMaxSkills to the console.
+
+By using Object.entries() with a for...of loop, we can iterate over the properties of the users object and find the person who has the most skills.
+ */
+
+
+for(const [user, usersdetails] of Object.entries(users)){
+  let loggedIn = usersdetails.isLoggedIn
+  // sum = 0
+  loggedIn === true
+  if(loggedIn){
+    Object.loggedIn
+  }
+console.log(loggedIn)
+}
+let userPoint = ''
+for(const [user, userDetails] of Object.keys(users)){
+  let points = userDetails.points
+  if(points >= 50){
+    userPoint = user
+  }
+  console.log(userPoint)
+}
+
+// 3. Find people who are MERN stack developer from the users object
+
+const mernStackDevelopers = [];
+
+for (const [user, userDetails] of Object.entries(users)) {
+  const skills = userDetails.skills;
+  if (
+    skills.includes('MongoDB') &&
+    skills.includes('Express') &&
+    skills.includes('React') &&
+    skills.includes('Node')
+  ) {
+    mernStackDevelopers.push(user);
+  }
+}
+console.log('MERN stack developers:', mernStackDevelopers);
+
+// 4. Set your name in the users object without modifying the original users object
+
+const fakeUserObject = Object.assign({}, users)
+for([user, userDetails] of Object.entries(users){
+  let myName = user.Alex
+})
+console.log(fakeUserObject)
--- DAY8/object.js
+++ DAY8/object.js
@@ -115,3 +115,41 @@ const person3 = {
   
   const copyPerson = Object.assign(person3, {})
   console.log(copyPerson)
+
+
+  const person5 = {
+    firstName: 'Asabeneh',
+    age: 250,
+    country: 'Finland',
+    city:'Helsinki',
+    skills: ['HTML', 'CSS', 'JS'],
+    title: 'teacher',
+    address: {
+      street: 'Heitamienkatu 16',
+      pobox: 2002,
+      city: 'Helsinki'
+    },
+    getPersonInfo: function() {
+      return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+    }
+  }
+  
+  //Object methods: Object.assign, Object.keys, Object.values, Object.entries
+  //hasOwnProperty
+  
+  const copyPerson5 = Object.assign({}, person5)
+  console.log(copyPerson5)
+  
+  const keys = Object.keys(copyPerson)
+  console.log(keys) //['firstName', 'age', 'country','city', 'skills','title', 'address', 'getPersonInfo']
+  const address = Object.keys(copyPerson.address)
+  console.log(address) //['street', 'pobox', 'city']
+  
+  const values = Object.values(copyPerson)
+  console.log(values)
+  
+  
+  const entries = Object.entries(copyPerson)
+  console.log(entries)
+  
+  
