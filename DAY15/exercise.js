f83a137cdad33e8118666405ce3885ca3aefdaec
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-07-27 20:41:50 +0100

    fixes

--- DAY15/exercise.js
+++ DAY15/exercise.js
@@ -25,3 +25,41 @@ const a = new animal('Lion', 5, 'brown', 4)
 console.log(a)
 console.log(a.getInfo())
 console.log(a.getColor())
+
+// console.log('%cExercise%c %cTwo%c %cStarts%c %cHere%c',
+//   'color:red;font-size:4rem',
+//   '',
+//   'color:green;font-size:4rem',
+//   '',
+//   'color:purple;font-style:italic;font-size:4rem',
+//   '',
+//   'font-size:4rem;color:yellow',
+//   '')
+
+class Dog extends animal{
+    constructor(name, age, color, legs){
+        super(name, age, color)
+            this.legs = legs
+        }
+        getDogtInfo(){
+            const checkLeg = this.legs === 4 ? `My dog has ${this.legs}` : `My dog is disable`
+            return `The name of my dog is ${this.name}. He is ${this.age} years old. ${this.color} in color, and has ${checkLeg}`
+        }
+    }
+
+const d = new Dog('Rex', 10, 'brown', 4)
+console.log(d.getDogtInfo())
+
+
+class Cat extends animal{
+    constructor(name, age, color, gender){
+        super(name, age, color)
+        this.gender = gender
+    }
+    getCatInfo(){
+        let checkGender = this.gender === 'He' ? `He` : 'she'
+        return `The name of my cat is ${this.name},${checkGender} is ${this.age} years old. ${checkGender} is ${this.color}. `
+    }
+}
+const c = new Cat('Muna', 20, 'brown', 'He')
+console.log(c.getCatInfo())
