c4e8d6d586efa2c1b6de92e51ce7523adedcb4dc
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-07-30 02:28:06 +0100

    fixes

---
+++ DAY16/json.js
@@ -0,0 +1,27 @@
+/**
+ * JSON stands for JavaScript Object Notation. The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text or string only. JSON is a light weight data format for storing and transporting. JSON is mostly used when data is sent from a server to a client. JSON is an easier-to-use alternative to XML.
+
+
+ */
+{
+    "users"  [
+        {
+            "firstName": "Mustapha",
+            "lastName": "Ahmad",
+            "age": 250,
+            "email": "ahmadishola@12"
+        },
+        {
+            "firstName":"Alex",
+            "lastName":"James",
+            "age":25,
+            "email":"alex@alex.com"
+          },
+        {
+        "firstName":"Lidiya",
+        "lastName":"Tekle",
+        "age":28,
+        "email":"lidiya@lidiya.com"
+        }
+    ]
+}
