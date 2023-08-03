034da09d76f4042966f00c8af31e84a3e7071037
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-08-03 02:59:34 +0100

    fixes

---
+++ DAY16/exercise.js
@@ -0,0 +1,145 @@
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}`
+
+/**
+ * Exercises Level 1
+1. Change skills array to JSON using JSON.stringify()
+2. Stringify the age variable
+3. Stringify the isMarried variable
+4. Stringify the student object
+ */
+
+// 1. 
+const skillsJason = JSON.stringify(skills, undefined, 3)
+console.log(skillsJason)
+// 2.
+const agesStringify = JSON.stringify(age, undefined, 3)
+console.log(agesStringify)
+// 3.
+const isMarriedstrigify = JSON.stringify(isMarried, undefined, 3)
+console.log(isMarriedstrigify)
+// 4.
+const studentObject = JSON.stringify(student, undefined, 4)
+console.log(studentObject)
+
+
+/**
+ * Exercises Level 2
+Stringify the students object with only firstName, lastName and skills properties
+ */
+
+const only = JSON.stringify(student, ['firstName', 'lastName', 'skills'], 4)
+console.log(only)
+
+/**
+ * Exercises Level 3
+Parse the txt JSON to object.
+Find the user who has many skills from the variable stored in txt.
+ */
+
+// 1. 
+const toObject = JSON.parse(txt, undefined, 3)
+console.log(toObject)
--- DAY16/json.js
+++ DAY16/json.js
@@ -3,25 +3,298 @@
 
 
  */
-{
-    "users"  [
-        {
-            "firstName": "Mustapha",
-            "lastName": "Ahmad",
-            "age": 250,
-            "email": "ahmadishola@12"
-        },
-        {
+
+
+// {
+//     "users":[
+//       {
+//         "firstName":"Asabeneh",
+//         "lastName":"Yetayeh",
+//         "age":250,
+//         "email":"asab@asb.com"
+//       },
+//       {
+//         "firstName":"Alex",
+//         "lastName":"James",
+//         "age":25,
+//         "email":"alex@alex.com"
+//       },
+//       {
+//       "firstName":"Lidiya",
+//       "lastName":"Tekle",
+//       "age":28,
+//       "email":"lidiya@lidiya.com"
+//       }
+//     ]
+//     }
+
+
+
+//     {
+//         "Alex": {
+//             "email": "alex@alex.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript"
+//             ],
+//             "age": 20,
+//             "isLoggedIn": false,
+//             "points": 30
+//         },
+//         "Asab": {
+//             "email": "asab@asab.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "Redux",
+//                 "MongoDB",
+//                 "Express",
+//                 "React",
+//                 "Node"
+//             ],
+//             "age": 25,
+//             "isLoggedIn": false,
+//             "points": 50
+//         },
+//         "Brook": {
+//             "email": "daniel@daniel.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "React",
+//                 "Redux"
+//             ],
+//             "age": 30,
+//             "isLoggedIn": true,
+//             "points": 50
+//         },
+//         "Daniel": {
+//             "email": "daniel@alex.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "Python"
+//             ],
+//             "age": 20,
+//             "isLoggedIn": false,
+//             "points": 40
+//         },
+//         "John": {
+//             "email": "john@john.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "React",
+//                 "Redux",
+//                 "Node.js"
+//             ],
+//             "age": 20,
+//             "isLoggedIn": true,
+//             "points": 50
+//         },
+//         "Thomas": {
+//             "email": "thomas@thomas.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "React"
+//             ],
+//             "age": 20,
+//             "isLoggedIn": false,
+//             "points": 40
+//         },
+//         "Paul": {
+//             "email": "paul@paul.com",
+//             "skills": [
+//                 "HTML",
+//                 "CSS",
+//                 "JavaScript",
+//                 "MongoDB",
+//                 "Express",
+//                 "React",
+//                 "Node"
+//             ],
+//             "age": 20,
+//             "isLoggedIn": false,
+//             "points": 40
+//         }
+    // }
+
+/**
+ * Converting JSON to JavaScript Object
+Mostly we fetch JSON data from HTTP response or from a file, but we can store the JSON as a string and we can change to Object for sake of demonstration.
+ In JavaScript the keyword JSON has parse() and stringify() methods.
+ When we want to change the JSON to an object we parse the JSON using JSON.parse(). When we want to change the object to JSON we use JSON.stringify().
+ */
+
+//  JSON.parse()
+// JSON.parse(json[, reviver])
+// json or text , the data
+// reviver is an optional callback function
+/* JSON.parse(json, (key, value) => {
+
+})
+*/
+
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+    
+    const usersObj = JSON.parse(usersText, undefined, 4)
+    console.log(usersObj)
+
+    const usersText2 = `{
+        "users":[
+          {
+            "firstName":"Asabeneh",
+            "lastName":"Yetayeh",
+            "age":250,
+            "email":"asab@asb.com"
+          },
+          {
             "firstName":"Alex",
             "lastName":"James",
             "age":25,
             "email":"alex@alex.com"
           },
-        {
-        "firstName":"Lidiya",
-        "lastName":"Tekle",
-        "age":28,
-        "email":"lidiya@lidiya.com"
-        }
-    ]
-}
+          {
+          "firstName":"Lidiya",
+          "lastName":"Tekle",
+          "age":28,
+          "email":"lidiya@lidiya.com"
+          }
+        ]
+        }`
+        
+        const usersObj2 = JSON.parse(usersText2, (key, value) => {
+          let newValue =
+            typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+          return newValue
+        })
+        console.log(usersObj2)
+
+// Converting Object to JSON
+// When we want to change the object to JSON we use JSON.stringify(). The stringify method takes one required parameter and two optional parameters. The replacer is used as filter and the space is an indentations. If we do not want to filter out any of the keys from the object we can just pass undefined.
+// JSON.stringify(obj, replacer, space)
+// json or text , the data
+// reviver is an optional callback function
+
+// Let us convert the following object to a string. First let use keep all the keys and also let us have 4 space indentation.
+
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'Redux',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+  
+  const txt = JSON.stringify(users, undefined, 4)
+  console.log(txt) // text means JSON- because json is a string form of an object.
+
+  /**
+   * Using a Filter Array with JSON.stringify
+Now, lets use the replacer as a filter. The user object has long list of keys but we are interested only in few of them.
+ We put the keys we want to keep in array as show in the example and use it the place of the replacer.
+   */
+
+ const user = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+  }
+  
+  const text = JSON.stringify(user,['firstName', 'lastName', 'country', 'city', 'age'],4)
+  console.log(text)
