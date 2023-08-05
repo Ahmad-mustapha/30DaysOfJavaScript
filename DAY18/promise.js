7e0ed50c9ae69b0a3da5ee35da6716426215906a
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-08-05 21:12:54 +0100

    fixes

---
+++ DAY18/promise.js
@@ -0,0 +1,44 @@
+// In this case the err has a value and it will return the err block.
+const doSomething = callback =>{
+    setTimeout(() =>{
+        const skills = ['HTML', 'CSS', 'JS']
+        callback('It did not go well', skills)
+    }, 2000)
+}
+
+const callback = (err, result) =>{
+    if(err){
+        return console.log(err)
+    }else{
+        return console.log(result)
+    }
+}
+doSomething(callback)
+
+// In this case the err is false and it will return the else block which is the result.
+
+const doSomething2 = callback =>{
+    setTimeout(() =>{
+        const skills = ['HTML', 'CSS', 'JS']
+        callback(false, skills)
+    }, 2000)
+}
+
+doSomething2((err, result) =>{
+    if(err){
+        return console.log(err)
+    }else{
+        return console.log(result)
+    }
+})
+
+// Promise constructor
+// We can create a promise using the Promise constructor. 
+// We can create a new promise using the key word new followed by the word Promise and followed by a parenthesis.
+//  Inside the parenthesis, it takes a callback function. The promise callback function has two parameters which are the resolve and reject functions.
+
+// syntax
+const promise = new Promise((resolve, reject) => {
+    resolve('success')
+    reject('failure')
+  })
