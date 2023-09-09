9bd4e1acd960c450abde0cbada1da7b9363b1b25
Ahmad Mustapha <ahmadishola12@gmail.com>
2023-09-09 10:08:17 +0100

    fixes

--- DAY18/promise.js
+++ DAY18/promise.js
@@ -1,6 +1,6 @@
 // In this case the err has a value and it will return the err block.
 const doSomething = (callback) =>{
-   callbackz
+   callback
 }
 const callback = ((a, b) =>{
     if(a > b){
@@ -27,13 +27,42 @@ doSomething2((err, result) =>{
     }
 })
 
+
+// CALLBACK FUNCTION
+document.getElementById("click-btn").addEventListener("click", myFunc)
+function myFunc(){
+    document.getElementById("demo").innerHTML = 'Hello world!!'
+}
+document.getElementById("click-btn").addEventListener("click", function() {
+    alert("Hello World!");
+  });
+
 // Promise constructor
 // We can create a promise using the Promise constructor. 
 // We can create a new promise using the key word new followed by the word Promise and followed by a parenthesis.
 //  Inside the parenthesis, it takes a callback function. The promise callback function has two parameters which are the resolve and reject functions.
 
 // syntax
-const promise = new Promise((resolve, reject) => {
+/*const promise = new Promise((resolve, reject) => {
     resolve('success')
     reject('failure')
-  })
+  })*/
+
+const promise = new Promise((resolve, reject) =>{
+    // Simulate a succesful operarion
+    setTimeout(() =>{
+        resolve('Success')
+    }, 1000)
+
+    // Simulate a failed operation
+    // setTimeout(() =>{
+    //     reject('failure')
+    // }, 1000)
+})
+promise.then((result) =>{
+    console.log('Resolved:', result)
+}).catch((error) =>{
+    console.log('Rejected:', error)
+})
+
+
